b632ce719b6cfe531a05c737b93eb9014cb6d3e7
Djaysson <djayson.dev@gmail.com>
2022-02-24 18:41:38 -0300

    Add animation to the why page section

--- src/Animation.js
+++ src/Animation.js
@@ -20,3 +20,16 @@ export const cardAnimation = {
   hidden: { scale: 0, opacity: 0 },
   show: { scale: 1, opacity: 1 },
 };
+
+export const topContainerAnimation = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+export const videoAnimation = {
+  hidden: { x: -800, opacity: 0 },
+  show: { x: 0, opacity: 1 },
+};
+export const reasonsAnimation = {
+  hidden: { x: 800, opacity: 0 },
+  show: { x: 0, opacity: 1 },
+};
--- src/sections/Why.jsx
+++ src/sections/Why.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import Title from "../components/Title";
 import { GoPlay } from "react-icons/go";
 
+import { motion } from "framer-motion";
+import { useScroll } from "../components/useScroll";
+import {
+  topContainerAnimation,
+  videoAnimation,
+  reasonsAnimation,
+} from "../Animation";
+
 import "../styles/sections/Why.scss";
 
 function Why() {
+  const [element, controls] = useScroll();
   return (
-    <div className="why-container" id="servicos">
+    <div className="why-container" id="servicos" ref={element}>
       <div className="container">
-        <div className="top">
+        <motion.div
+          className="top"
+          variants={topContainerAnimation}
+          animate={controls}
+          transition={{ duration: 1 }}
+        >
           <Title title="Por que Empire ?" color="pink" lineCenter={true} />
           <div className="subTitle">
             <p>
@@ -17,14 +31,26 @@ function Why() {
               fornecendo as melhores soluções para suas necessidades.
             </p>
           </div>
-        </div>
+        </motion.div>
         <div className="content">
-          <div>
-            <div className="video">
-              <GoPlay />
+          <motion.div
+            className=""
+            variants={videoAnimation}
+            animate={controls}
+            transition={{ type: "tween", duration: 0.5 }}
+          >
+            <div>
+              <div className="video">
+                <GoPlay />
+              </div>
             </div>
-          </div>
-          <div className="reasons">
+          </motion.div>
+          <motion.div
+            className="reasons"
+            variants={reasonsAnimation}
+            animate={controls}
+            transition={{ type: "tween", duration: 0.5 }}
+          >
             <ul>
               <li>Mais de 10 anos de experiência em todo o setor</li>
               <li>
@@ -38,7 +64,7 @@ function Why() {
                 dias por semana
               </li>
             </ul>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
